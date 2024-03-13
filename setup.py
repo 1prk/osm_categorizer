c72ea70bfc0061a704ca9b1acb7c7179d7487673
Porojkow <iwan.porojkow@tu-dresden.de>
2024-03-13 11:56:31 +0100

    test

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ with open("netapy/__init__.py") as f:
       version = line.split("=")[1].strip().strip('"').strip('"')
       continue
 
+#test
 # The text of the README file.
 try:
   with open(here / "README.md") as f:
