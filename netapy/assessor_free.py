8c84fb7dd885c50b3d1f781589efb185a2c81f69
lukawska <miroslawa.lukawska@tu-dresden.de>
2024-06-20 16:38:54 +0200

    set_value for single categories added

--- netapy/assessor_free.py
+++ netapy/assessor_free.py
@@ -39,7 +39,7 @@ class Assessor():
         # sr_buffer_tags = sr_buffer_tags[dropcol]
         raise NotImplementedError
 
-    def set_value(self, x):
+    def set_value(self, x, sides = "double"):
         is_reversed = x["reversed"]
         is_segregated = any(key for key, value in x.items() if 'segregated' in key and value == 'yes') #and no "nos" in segregated, zB 4746913 hat cycleway:left:segregated = no !
         is_footpath = x["highway"] in ["footway", "pedestrian"]
@@ -157,6 +157,7 @@ class Assessor():
                             or x["cycleway:both"] in ["lane", "shared_lane"]
                             or any(key for key, value in x.items() if 'left:lane' in key and value in ['exclusive']))
 
+
         ##schutzstreifen/radfahrstreifen
         ##bus
         is_buslane_right = (x["cycleway"] == "share_busway"
@@ -166,10 +167,10 @@ class Assessor():
                            or x["cycleway:left"] == "share_busway"
                            or x["cycleway:both"] == "share_busway")
 
+
         ## bicycle_way
         # First option: "bicycle_way_right"
         conditions_b_way_right = [
-            # is_bikeroad,
             is_bikepath_right and not can_walk_right,  # 0 and 1
             is_bikepath_right and is_segregated,  # 0 and 2
             can_bike and (is_path or is_track) and not can_walk_right,  # and not is_footpath, #3, 4, 1
@@ -179,7 +180,6 @@ class Assessor():
         ]
 
         conditions_b_way_left = [
-            # is_bikeroad,
             is_bikepath_left and not can_walk_left,  # b_way_left_0
             is_bikepath_left and is_segregated,  # b_way_left_1
             can_bike and (is_path or is_track) and not can_walk_left,  # and not is_footpath, #3, 4, 1
@@ -212,178 +212,238 @@ class Assessor():
             and not is_path and not is_track and not cannot_bike,
         ]
 
-        def get_infra(self, x):
+        if sides == "double":
+            def get_infra(self, x):
 
-            if ('access' in x.index and is_not_accessible) or ('tram' in x.index and x['tram'] == 'yes'):
-                return 'no'  # unpacked from "service"
-            # remove service right away
+                if ('access' in x.index and is_not_accessible) or ('tram' in x.index and x['tram'] == 'yes'):
+                    return 'no'  # unpacked from "service"
+                # remove service right away
 
-            if is_service:
-                return "service_misc"
+                if is_service:
+                    return "service_misc"
 
-            if is_cycle_highway:
-                return "cycle_highway"
+                if is_cycle_highway:
+                    return "cycle_highway"
 
-            #### 3 # new option: "bicycle_road"
-            if is_bikeroad:
-                return "bicycle_road"
+                #### 3 # new option: "bicycle_road"
+                if is_bikeroad:
+                    return "bicycle_road"
 
-            #### 1
-            elif any(conditions_b_way_right):
-                if any(conditions_b_way_left):
-                    return "bicycle_way_both"
+                #### 1
+                elif any(conditions_b_way_right):
+                    if any(conditions_b_way_left):
+                        return "bicycle_way_both"
+                    elif is_bikelane_left:
+                        return "bicycle_way_right_lane_left"
+                    elif is_buslane_left:
+                        return "bicycle_way_right_bus_left"
+                    elif any(conditions_mixed_left):
+                        return "bicycle_way_right_mixed_left"
+                    elif any(conditions_mit_left):
+                        return "bicycle_way_right_mit_left"
+                    elif is_pedestrian_left:
+                        return "bicycle_way_right_pedestrian_left"
+                    else:
+                        return "bicycle_way_right_no_left"
+
+                elif any(conditions_b_way_left):
+                    if is_bikelane_right:
+                        return "bicycle_way_left_lane_right"
+                    elif is_buslane_right:
+                        return "bicycle_way_left_bus_right"
+                    elif any(conditions_mixed_right):
+                        return "bicycle_way_left_mixed_right"
+                    elif any(conditions_mit_right):
+                        return "bicycle_way_left_mit_right"
+                    elif is_pedestrian_right:
+                        return "bicycle_way_left_pedestrian_right"
+                    else:
+                        return "bicycle_way_left_no_right"
+
+                #### 4 # Third option: "bicycle_lane"
+                elif is_bikelane_right:
+                    if is_bikelane_left:
+                        return "bicycle_lane_both"
+                    elif is_buslane_left:
+                        return "bicycle_lane_right_bus_left"
+                    elif any(conditions_mixed_left):
+                        return "bicycle_lane_right_mixed_left"
+                    elif any(conditions_mit_left):
+                        return "bicycle_lane_right_mit_left"
+                    elif is_pedestrian_left:
+                        return "bicycle_lane_right_pedestrian_left"
+                    else:
+                        return "bicycle_lane_right_no_left"
+
                 elif is_bikelane_left:
-                    return "bicycle_way_right_lane_left"
-                elif is_buslane_left:
-                    return "bicycle_way_right_bus_left"
-                elif any(conditions_mixed_left):
-                    return "bicycle_way_right_mixed_left"
-                elif any(conditions_mit_left):
-                    return "bicycle_way_right_mit_left"
-                elif is_pedestrian_left:
-                    return "bicycle_way_right_pedestrian_left"
-                else:
-                    return "bicycle_way_right_no_left"
+                    if is_buslane_right:
+                        return "bicycle_lane_left_bus_right"
+                    elif any(conditions_mixed_right):
+                        return "bicycle_lane_left_mixed_right"
+                    elif any(conditions_mit_right):
+                        return "bicycle_lane_left_mit_right"
+                    elif is_pedestrian_right:
+                        return "bicycle_lane_left_pedestrian_right"
+                    else:
+                        return "bicycle_lane_left_no_right"
 
-            elif any(conditions_b_way_left):
-                if is_bikelane_right:
-                    return "bicycle_way_left_lane_right"
+                #### 5 # Fourth option: "bus_lane"
                 elif is_buslane_right:
-                    return "bicycle_way_left_bus_right"
-                elif any(conditions_mixed_right):
-                    return "bicycle_way_left_mixed_right"
-                elif any(conditions_mit_right):
-                    return "bicycle_way_left_mit_right"
-                elif is_pedestrian_right:
-                    return "bicycle_way_left_pedestrian_right"
-                else:
-                    return "bicycle_way_left_no_right"
+                    if is_buslane_left:
+                        return "bus_lane_both"
+                    elif any(conditions_mixed_left):
+                        return "bus_lane_right_mixed_left"
+                    elif any(conditions_mit_left):
+                        return "bus_lane_right_mit_left"
+                    elif is_pedestrian_left:
+                        return "bus_lane_right_pedestrian_left"
+                    else:
+                        return "bus_lane_right_no_left"
 
-            #### 4 # Third option: "bicycle_lane"
-            elif is_bikelane_right:
-                if is_bikelane_left:
-                    return "bicycle_lane_both"
                 elif is_buslane_left:
-                    return "bicycle_lane_right_bus_left"
-                elif any(conditions_mixed_left):
-                    return "bicycle_lane_right_mixed_left"
-                elif any(conditions_mit_left):
-                    return "bicycle_lane_right_mit_left"
-                elif is_pedestrian_left:
-                    return "bicycle_lane_right_pedestrian_left"
-                else:
-                    return "bicycle_lane_right_no_left"
+                    if any(conditions_mixed_right):
+                        return "bus_lane_left_mixed_right"
+                    elif any(conditions_mit_right):
+                        return "bus_lane_left_mit_right"
+                    elif is_pedestrian_right:
+                        return "bus_lane_left_pedestrian_right"
+                    else:
+                        return "bus_lane_left_no_right"
 
-            elif is_bikelane_left:
-                if is_buslane_right:
-                    return "bicycle_lane_left_bus_right"
+
+                #### 2
                 elif any(conditions_mixed_right):
-                    return "bicycle_lane_left_mixed_right"
-                elif any(conditions_mit_right):
-                    return "bicycle_lane_left_mit_right"
-                elif is_pedestrian_right:
-                    return "bicycle_lane_left_pedestrian_right"
-                else:
-                    return "bicycle_lane_left_no_right"
+                    if any(conditions_mixed_left):
+                        return "mixed_way_both"
+                    elif any(conditions_mit_left):
+                        return "mixed_way_right_mit_left"
+                    elif is_pedestrian_left:
+                        return "mixed_way_right_pedestrian_left"
+                    else:
+                        return "mixed_way_right_no_left"
 
-            #### 5 # Fourth option: "bus_lane"
-            elif is_buslane_right:
-                if is_buslane_left:
-                    return "bus_lane_both"
                 elif any(conditions_mixed_left):
-                    return "bus_lane_right_mixed_left"
-                elif any(conditions_mit_left):
-                    return "bus_lane_right_mit_left"
-                elif is_pedestrian_left:
-                    return "bus_lane_right_pedestrian_left"
-                else:
-                    return "bus_lane_right_no_left"
+                    if any(conditions_mit_right):
+                        return "mixed_way_left_mit_right"
+                    elif is_pedestrian_right:
+                        return "mixed_way_left_pedestrian_right"
+                    else:
+                        return "mixed_way_left_no_right"
 
-            elif is_buslane_left:
-                if any(conditions_mixed_right):
-                    return "bus_lane_left_mixed_right"
+                #### 6
                 elif any(conditions_mit_right):
-                    return "bus_lane_left_mit_right"
-                elif is_pedestrian_right:
-                    return "bus_lane_left_pedestrian_right"
-                else:
-                    return "bus_lane_left_no_right"
+                    if any(conditions_mit_left):
+                        return "mit_road_both"
+                    elif is_pedestrian_left:
+                        return "mit_road_right_pedestrian_left"
+                    else:
+                        return "mit_road_right_no_left"
 
-
-            #### 2
-            elif any(conditions_mixed_right):
-                if any(conditions_mixed_left):
-                    return "mixed_way_both"
                 elif any(conditions_mit_left):
-                    return "mixed_way_right_mit_left"
-                elif is_pedestrian_left:
-                    return "mixed_way_right_pedestrian_left"
-                else:
-                    return "mixed_way_right_no_left"
+                    if is_pedestrian_right:
+                        return "mit_road_left_pedestrian_right"
+                    else:
+                        return "mit_road_left_no_right"
 
-            elif any(conditions_mixed_left):
-                if any(conditions_mit_right):
-                    return "mixed_way_left_mit_right"
-                elif is_pedestrian_right:
-                    return "mixed_way_left_pedestrian_right"
-                else:
-                    return "mixed_way_left_no_right"
+                #### 8
+                elif is_pedestrian_right and (not 'indoor' in x.values or (x['indoor'] != 'yes')):
+                    if is_pedestrian_left and (not 'indoor' in x.values or (x['indoor'] != 'yes')):
+                        if 'access' in x.index and x['access'] == 'customers':
+                            return "no"
+                        else:
+                            return "pedestrian_both"
+                    else:
+                        return "pedestrian_right_no_left"
 
-            #### 6
-            elif any(conditions_mit_right):
-                if any(conditions_mit_left):
-                    return "mit_road_both"
-                elif is_pedestrian_left:
-                    return "mit_road_right_pedestrian_left"
-                else:
-                    return "mit_road_right_no_left"
 
-            elif any(conditions_mit_left):
-                if is_pedestrian_right:
-                    return "mit_road_left_pedestrian_right"
-                else:
-                    return "mit_road_left_no_right"
-
-            #### 8
-            elif is_pedestrian_right and (not 'indoor' in x.values or (x['indoor'] != 'yes')):
-                if is_pedestrian_left and (not 'indoor' in x.values or (x['indoor'] != 'yes')):
+                elif is_pedestrian_left and (not 'indoor' in x.values or (x['indoor'] != 'yes')):
                     if 'access' in x.index and x['access'] == 'customers':
                         return "no"
                     else:
-                        return "pedestrian_both"
+                        return "pedestrian_left_no_right"
+
+                elif is_path_not_forbidden:
+                    return "path_not_forbidden"
+
+                #### Fallback option: "no"
                 else:
-                    return "pedestrian_right_no_left"
-
-
-            elif is_pedestrian_left and (not 'indoor' in x.values or (x['indoor'] != 'yes')):
-                if 'access' in x.index and x['access'] == 'customers':
                     return "no"
-                else:
-                    return "pedestrian_left_no_right"
 
-            elif is_path_not_forbidden:
-                return "path_not_forbidden"
+            cat = None
+            cat = get_infra(x)
+            # making sure that the variable cat has been filled
+            assert (isinstance(cat, str))
 
-            #### Fallback option: "no"
-            else:
-                return "no"
-
-        cat = None
-        cat = get_infra(x)
-        # making sure that the variable cat has been filled
-        assert (isinstance(cat, str))
-
-        if ("_both" in cat) or (
-                cat in ["no", "cycle_highway", "bicycle_road", "path_not_forbidden", "service_misc"]):
-            return cat
-        else:
-            # for categories with "right & left" - revert if needed
-            if not is_reversed:
+            if ("_both" in cat) or (
+                    cat in ["no", "cycle_highway", "bicycle_road", "path_not_forbidden", "service_misc"]):
                 return cat
             else:
-                sides = ["left", "right"] if cat.split("_")[-1] == "right" else ["right", "left"]
-                for side in sides:
-                    cat = " ".join(cat.split(side))
+                # for categories with "right & left" - revert if needed
+                if not is_reversed:
+                    return cat
+                else:
+                    sides = ["left", "right"] if cat.split("_")[-1] == "right" else ["right", "left"]
+                    for side in sides:
+                        cat = " ".join(cat.split(side))
 
-                res = cat.split()
-                return res[0] + sides[1] + res[1] + sides[0]
+                    res = cat.split()
+                    return res[0] + sides[1] + res[1] + sides[0]
+
+        else:
+            assert(sides == "single")
+
+            def get_infra(self, x):
+
+                if ('access' in x.index and is_not_accessible) or ('tram' in x.index and x['tram'] == 'yes'):
+                    return 'no'  # unpacked from "service"
+                # remove service right away
+
+                if is_service:
+                    return "service_misc"
+
+                if is_cycle_highway:
+                    return "cycle_highway"
+
+                #### 3 # new option: "bicycle_road"
+                if is_bikeroad:
+                    return "bicycle_road"
+
+                #### 1
+                elif any(conditions_b_way_left) or any(conditions_b_way_right):
+                    return "bicycle_way"
+
+                #### 4 # Third option: "bicycle_lane"
+                elif is_bikelane_left or is_bikelane_right:
+                    return "bicycle_lane"
+
+                #### 5 # Fourth option: "bus_lane"
+                elif is_buslane_left or is_buslane_right:
+                    return "bus_lane"
+
+                #### 2
+                elif any(conditions_mixed_left) or any(conditions_mixed_right):
+                    return "mixed_way"
+
+                #### 6
+                elif any(conditions_mit_left) or any(conditions_mit_right):
+                    return "mit_road"
+
+                elif is_pedestrian_left or is_pedestrian_right and (not 'indoor' in x.values or (x['indoor'] != 'yes')):
+                    if 'access' in x.index and x['access'] == 'customers':
+                        return "no"
+                    else:
+                        return "pedestrian"
+
+                elif is_path_not_forbidden:
+                    return "path_not_forbidden"
+
+                #### Fallback option: "no"
+                else:
+                    return "no"
+
+            cat = None
+            cat = get_infra(x)
+            # making sure that the variable cat has been filled
+            assert (isinstance(cat, str))
+
+            return cat
